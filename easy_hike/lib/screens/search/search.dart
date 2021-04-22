be36a9687b239813112f706263773f89ab5e705e
Dishank Kalra <dishankalra@gmail.com>
2021-04-22 20:17:09 +0530

    Added Drawer

--- easy_hike/lib/screens/search/search.dart
+++ easy_hike/lib/screens/search/search.dart
@@ -20,6 +20,73 @@ class _MainSearchState extends State<MainSearch> {
     return ScopedModel(
       model: locator<AuthModel>(),
       child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30 // To be decided later
+                  ),
+              DrawerHeader(
+                child: Container(
+                  // TO DO: Image(Have to be decided)
+                  height: 70,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset("assets/logo_dark.png"),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'To be Decided',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height:45),
+              Text(
+                'To be Decided',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),   
+              SizedBox(height:45),
+              Text(
+                'To be Decided',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),  
+              SizedBox(height:45),
+              Text(
+                'To be Decided',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height:45),
+              Material(
+                borderRadius: BorderRadius.circular(500),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(500),
+                  splashColor: Colors.black45,
+                   onTap: () {
+                    Navigator.of(context).pop(); // To DO: close the drawer
+                  },
+                ),
+              ),                                         
+            ],
+          ),
+        ),
         backgroundColor: Colors.white,
         appBar: AppBar(
           elevation: 0,
@@ -151,3 +218,4 @@ class _MainSearchState extends State<MainSearch> {
     );
   }
 }
+
