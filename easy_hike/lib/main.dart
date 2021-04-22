37329170312fc261bfedec7c3c5a4af9c6ee4924
Dishank Kalra <dishankalra@gmail.com>
2021-04-22 18:58:25 +0530

    wip_settings_feature

--- easy_hike/lib/main.dart
+++ easy_hike/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_hike/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
+// Branch check
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
