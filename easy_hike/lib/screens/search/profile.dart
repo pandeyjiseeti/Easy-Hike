dd701cecb943d2a85c0a69176b5bd0d18f5e478a
jainakki16 <akshatjain981000@gmail.com>
2021-04-24 13:01:15 +0530

    added profile photo

--- easy_hike/lib/screens/search/profile.dart
+++ easy_hike/lib/screens/search/profile.dart
@@ -10,7 +10,33 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
+      ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Center(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.network(
+                          'https://crhscountyline.com/wp-content/uploads/2020/03/Capture.png',
+                          width: 150.0,
+                          height: 150.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
