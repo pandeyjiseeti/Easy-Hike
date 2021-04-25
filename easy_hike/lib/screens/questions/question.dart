da995652450fe7f62fdb377c37bedf57651bea59
Anup <mishraanup500@gmail.com>
2021-04-25 20:56:22 +0530

    added various text input fields

--- easy_hike/lib/screens/questions/question.dart
+++ easy_hike/lib/screens/questions/question.dart
@@ -12,7 +12,36 @@ class _QuestionState extends State<Question> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Current/Last job position",
+                    labelText: "Job Position",
+                    border: UnderlineInputBorder()),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Company Name",
+                    labelText: "Company Name",
+                    border: UnderlineInputBorder()),
+              ),
+
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Industry Name",
+                    labelText: "Industry",
+                    border: UnderlineInputBorder()),
+              ),
+
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Company Name",
+                    labelText: "Company Name",
+                    border: UnderlineInputBorder()),
+              ),
+          ],
+        ),
     );
   }
 }
