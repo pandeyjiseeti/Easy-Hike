1251657d5f7928c7e3c4acf6c1c06afdf1a4ad53
Titas Das <dastitas21@gmail.com>
2021-04-25 23:52:31 +0530

    Added Design to the fields

--- easy_hike/lib/screens/questions/question.dart
+++ easy_hike/lib/screens/questions/question.dart
@@ -8,7 +8,8 @@ class Question extends StatefulWidget {
 }
 
 class _QuestionState extends State<Question> {
-
+  bool _visible = false;
+  bool _visible2 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +18,7 @@ class _QuestionState extends State<Question> {
         elevation: 0,
         automaticallyImplyLeading: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           color: Colors.black,
           onPressed: () => {
             // Navigator.push(
@@ -32,7 +33,7 @@ class _QuestionState extends State<Question> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (BuildContext context) => HomePage()),
+                    builder: (BuildContext context) => MainSearch()),
               ),
             },
             child: Text(
@@ -44,34 +45,72 @@ class _QuestionState extends State<Question> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextButton(
+              child: Text("Job Description"),
+              onPressed: () {
+                setState(() {
+                  _visible = !_visible;
+                });
+              },
+            ),
+            Visibility(
+              visible: _visible,
               child: TextField(
                 decoration: InputDecoration(
                     hintText: "Current/Last job position",
                     labelText: "Job Position",
                     border: UnderlineInputBorder()),
               ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Visibility(
+              visible: _visible,
               child: TextField(
                 decoration: InputDecoration(
                     hintText: "Enter Company Name",
                     labelText: "Company Name",
                     border: UnderlineInputBorder()),
               ),
-
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Visibility(
+              visible: _visible,
               child: TextField(
                 decoration: InputDecoration(
                     hintText: "Enter Industry Name",
                     labelText: "Industry",
                     border: UnderlineInputBorder()),
               ),
-
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Text("Years of Experience"),
+                onPressed: () {
+                  setState(() {
+                    _visible2 = !_visible2;
+                  });
+                },
+              ),
+            ),
+            Visibility(
+              visible: _visible2,
               child: TextField(
                 decoration: InputDecoration(
                     hintText: "Enter Company Name",
                     labelText: "Company Name",
                     border: UnderlineInputBorder()),
               ),
+            ),
           ],
         ),
+      ),
     );
   }
 }
