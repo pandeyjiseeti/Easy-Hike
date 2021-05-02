cf98cc45d641a69ec44c6a1bac2d546d4a377622
Anup <mishraanup500@gmail.com>
2021-05-02 12:50:52 +0530

    updated personal intro

--- easy_hike/lib/screens/questions/personalintro.dart
+++ easy_hike/lib/screens/questions/personalintro.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class PersonalIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextButton(
+              child: Text("Personal Informations"),
+              onPressed: () {
+              },
+            ),
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter your name",
+                    labelText: "Name",
+                    border: UnderlineInputBorder()),
+              ),
+            SizedBox(
+              height: 16,
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
