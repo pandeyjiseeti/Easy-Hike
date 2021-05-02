4c800a7347f2bc65c540ab79048aba91495f291a
Anup <mishraanup500@gmail.com>
2021-05-02 13:34:42 +0530

    added field

--- easy_hike/lib/screens/questions/personalintro.dart
+++ easy_hike/lib/screens/questions/personalintro.dart
@@ -22,7 +22,51 @@ class PersonalIntro extends StatelessWidget {
             SizedBox(
               height: 16,
             ),
-
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Email address",
+                    labelText: "Email Address",
+                    border: UnderlineInputBorder()),
+              ),
+              SizedBox(
+              height: 16,
+            ),
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter phone number",
+                    labelText: "Phone no",
+                    border: UnderlineInputBorder()),
+              ),
+            SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Permanent address",
+                    labelText: "Permenant Address",
+                    border: UnderlineInputBorder()),
+              ),
+              SizedBox(
+              height: 16,
+            ),
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter residential address",
+                    labelText: "Residential Address",
+                    border: UnderlineInputBorder()),
+              ),
+              SizedBox(
+              height: 16,
+            ),
+            TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter residential address",
+                    labelText: "Residential Address",
+                    border: UnderlineInputBorder()),
+              ),
           ],
         ),
       ),
